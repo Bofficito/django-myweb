08ecb40f081bb36d1df342206ee69b5a2bc6088d
Lautaro Boffi <lautaroboffi@gmail.com>
2025-03-14 18:52:11 -0300

    allowed host repair

--- home/settings.py
+++ home/settings.py
@@ -132,4 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
-ALLOWED_HOSTS = ['django-myweb-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
